9c23f9d1cb33308a98f14c91b6b4a886df8fda02
SavageSides <42398425+SavageSides@users.noreply.github.com>
2018-09-12 21:01:57 -0500

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -329,7 +329,7 @@ async def help(ctx):
     embed.set_footer(text='Reminder: All commands need to be lower case!')
     await client.send_message(author, embed=embed)
     embed = discord.Embed(color=0x36393E)
-    embed.add_filed(name='We have sent you a message!', value='Please check direct Messages!', inline=False)
+    embed.add_field(name='We have sent you a message!', value='Please check direct Messages!', inline=False)
     await client.say(embed=embed)
     
     
